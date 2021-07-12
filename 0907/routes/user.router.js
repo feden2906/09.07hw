06800f620934bca3ee847a9b9de745d89758da78
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-07-12 10:11:20 +0300

    0907p1

--- 0907/routes/user.router.js
+++ 0907/routes/user.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { nameConstants: { CREATE_USER, UPDATE_USER } } = require('../constants');
 const { userController } = require('../controllers');
-const { userMiddlewar } = require('../middlewars');
+const { authMiddlewar, userMiddlewar } = require('../middlewars');
 
 router.post('/', userMiddlewar.checkDataValidity(CREATE_USER), userMiddlewar.checkEmailBusy, userController.createUser);
 router.get('/', userController.getAllUsers);
@@ -10,7 +10,12 @@ router.get('/', userController.getAllUsers);
 router.use('/:userId', userMiddlewar.getUserByDynamicParam('userId', 'params', '_id'));
 
 router.get('/:userId', userController.getUserById);
-router.put('/:userId', userMiddlewar.checkDataValidity(UPDATE_USER), userController.updateUserById);
-router.delete('/:userId', userController.deleteUserById);
+router.put(
+  '/:userId',
+  authMiddlewar.checkAccessToken,
+  userMiddlewar.checkDataValidity(UPDATE_USER),
+  userController.updateUserById
+);
+router.delete('/:userId', authMiddlewar.checkAccessToken, userController.deleteUserById);
 
 module.exports = router;
